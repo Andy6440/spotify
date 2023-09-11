22c4e0c52c6969a997dd35a78710a01ca878719e
ANDREA ALVARADO <andreassilva0108@gmail.com>
2023-09-10 21:58:26 -0300

    add comment and documentation on Auth routes

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -3,19 +3,26 @@ import { Request, Response,NextFunction } from 'express'
 import AuthenticationError from '../models/errors/AuthenticationError'
 import UserService from '../services/db/User/User.service'
 
+/**
+ * Middleware to handle authentication
+ * 
+ * @param req - The request object
+ * @param res - The response object
+ * @param next - The next function to call
+ */
 const authenticationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    // Verifica si la cookie existe
     if (!req.cookies.access_token) {   
         const id = process.env.USER_ID as string    
         const user = await UserService.findUser(id)
-        if(user){
+
+        if (user) {
             res.cookie('access_token', user.token, { maxAge: 900000, httpOnly: true })
-        }else {
-            new AuthenticationError('User not found')
+        } else {
+            throw new AuthenticationError('User not found')
         }
     }       
 
-    next() // Continúa con el siguiente middleware o ruta
+    next()
 }
 
 export default authenticationMiddleware
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -10,17 +10,37 @@ const authRoutes = express.Router()
  * 
  */
 
-// Route: GET /login
-// Description: Endpoint for user login
+/**
+ * User login endpoint.
+ * 
+ * @route GET /login
+ * 
+ * @example
+ * GET /login
+ */
 authRoutes.get('/login',login)
 
-// Route: GET /callback
-// Description: Endpoint for callback after user authentication
+/**
+ * Callback endpoint after user authentication.
+ * 
+ * @route GET /callback
+ * @param {string} code - Authorization code required for the callback, from the query parameter.
+ * 
+ * @example
+ * GET /callback?code=YOUR_AUTH_CODE_HERE
+ */
 authRoutes.get('/callback', validateRequiredParam('code'), callback)
 
+/**
+ * Apply authentication to all `authRoutes`.
+ */
 authRoutes.use(authenticationMiddleware)
-// Route: GET /callback
-// Description: Endpoint for callback after user authentication
+/**
+ * Retrieve the authenticated user's profile.
+ * 
+ * @route GET /user
+ * @param {string} access_token - The authentication token. Found in the cookies.
+ */
 authRoutes.get('/user',  UserProfile)
 
 export default authRoutes
